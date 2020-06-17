b54f2df35d47b688516210230a7fc76c99424d6a
Mateusz Rzewnicki <ootexh@wp.pl>
2020-06-17 17:26:48 +0200

    Update UserDetailsServiceImpl.java
    Privilages not added to new user

--- src/main/java/com/cookingbook/service/UserDetailsServiceImpl.java
+++ src/main/java/com/cookingbook/service/UserDetailsServiceImpl.java
@@ -26,13 +26,15 @@ public class UserDetailsServiceImpl implements UserDetailsService{
     public UserDetails loadUserByUsername(String username) {
         User user = userRepository.findByUsername(username);
         if (user == null) throw new UsernameNotFoundException(username);
-
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        for (Role role : user.getRoles()){
-            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        try {
+	        for (Role role : user.getRoles()){
+	            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+	        }
+	        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+        }catch (Exception  e){
+            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
         }
-
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
     }
 
 }
